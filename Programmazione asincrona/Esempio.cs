76c7a8613d1cf6fded87224e07c85a595a048b80
Damiano-dev <damianocr@gmail.com>
2020-08-11 16:42:38 +0200

    Aggiungere i file di progetto.

---
+++ Programmazione asincrona/Esempio.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Programmazione_asincrona
+{
+    public class Esempio
+    {
+        const int LOWERBOUND = 0;
+        const int UPPERBOUND = 1001;
+
+        static Object lockObj = new Object();
+        static Random rnd = new Random();
+        static CountdownEvent cte;
+
+        static int totalCount = 0;
+        static int totalMidpoint = 0;
+        static int midpointCount = 0;
+
+        public static void Main()
+        {
+            cte = new CountdownEvent(1);
+            // Start three threads. 
+            for (int ctr = 0; ctr <= 2; ctr++)
+            {
+                cte.AddCount();
+                Thread th = new Thread(GenerateNumbers);
+                th.Name = "Thread" + ctr.ToString();
+                th.Start();
+            }
+            cte.Signal();
+            cte.Wait();
+            Console.WriteLine();
+            Console.WriteLine("Total midpoint values:  {0,10:N0} ({1:P3})",
+                              totalMidpoint, totalMidpoint / ((double)totalCount));
+            Console.WriteLine("Total number of values: {0,10:N0}",
+                              totalCount);
+        }
+
+        private static void GenerateNumbers()
+        {
+            int midpoint = (UPPERBOUND - LOWERBOUND) / 2;
+            int value = 0;
+            int total = 0;
+            int midpt = 0;
+
+            do
+            {
+                lock (lockObj)
+                {
+                    value = rnd.Next(LOWERBOUND, UPPERBOUND);
+                }
+                if (value == midpoint)
+                {
+                    Interlocked.Increment(ref midpointCount);
+                    midpt++;
+                }
+                total++;
+            } while (midpointCount < 10000);
+
+            Interlocked.Add(ref totalCount, total);
+            Interlocked.Add(ref totalMidpoint, midpt);
+
+            string s = String.Format("Thread {0}:\n", Thread.CurrentThread.Name) +
+                       String.Format("   Random Numbers: {0:N0}\n", total) +
+                       String.Format("   Midpoint values: {0:N0} ({1:P3})", midpt,
+                                     ((double)midpt) / total);
+            Console.WriteLine(s);
+            cte.Signal();
+        }
+    }
+}
---
+++ Programmazione asincrona/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Programmazione_asincrona
+{
+    class Program
+    {
+        static Thread thread1, thread2;
+        static CountdownEvent cte;
+
+
+        public static void Main()
+        {
+            int n = 0;
+            Object obj = new Object(); //l'oggetto deve essere privato altrimente si può usare da altre classi per bloccare altri thread
+            
+            //thread1 = new Thread(ThreadProc);
+            //thread1.Name = "Thread1";
+            //thread1.Start();
+
+            //thread2 = new Thread(ThreadProc);
+            //thread2.Name = "Thread2";
+            //thread2.Start();
+            //thread2.Join(); // il main si blocca per far finire il thread 2, infatti è il main che invoca il join
+
+            for (int i = 0; i < 5; i++)
+            {
+               
+                Thread th = new Thread(() =>
+                {
+                    //lock (obj)
+                    //{
+                        Interlocked.Increment(ref n);
+                    //n = n + 1;
+                    Thread.Sleep(2000);
+                    Thread.CurrentThread.Name = "Thread" + i;
+                        Console.WriteLine("n={0}, thread={1}",  n, Thread.CurrentThread.Name);
+                    
+                    //}
+
+
+                });
+
+                th.Start();
+            }
+
+            
+
+            Console.ReadLine();
+        }
+
+        private static void ThreadProc()
+        {
+            Console.WriteLine(Thread.CurrentThread.Name);
+
+            // il thread 1 invoca il join del thread 2 e si blocca, il thread due poi invoca il suo stesso thread per bloccarsi, quindi ci vuole un if per bloccare solo il thread che  chiama il join
+            //thread2.Join();
+
+
+            //if (Thread.CurrentThread == thread1)
+            //{
+            //    if (thread2.Join(10000)) // Per il tempo indicato il thread 2 blocca il thread 1
+            //    {
+            //        Console.WriteLine("il secondo thread è finito");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Continua il thread 1");
+            //    }
+            //}
+
+
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.WriteLine(i+ " "+ Thread.CurrentThread.Name);
+            //}
+
+        }
+    }
+}
